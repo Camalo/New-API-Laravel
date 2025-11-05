b5c5d2b41adb3952b731e23b7c083eb5bd866f3d
Алсу Камалова <camalovalsu@outlook.com>
2025-11-06 01:11:10 +0300

    Технические файлы

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -15,4 +17,20 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+
+    })
+    ->withProviders([
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
+        Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+        Illuminate\Encryption\EncryptionServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Session\SessionServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class
+    ])->create();
+
+// $app->singleton('files', function () {
+//     return new Illuminate\Filesystem\Filesystem;
+// });
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Infrastructure\Http\BalanceController;
+use App\Infrastructure\Http\DepositController;
+use App\Infrastructure\Http\TransferController;
+use App\Infrastructure\Http\WithdrawController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post(
+    '/deposit',
+    DepositController::class
+);
+Route::post(
+    '/withdraw',
+    WithdrawController::class
+);
+Route::post(
+    '/transfer',
+    TransferController::class
+);
+Route::get(
+    '/balance/{user_id}',
+    BalanceController::class
+);
